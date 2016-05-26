36d2a7a3ba0c1e9161937b92b7a1ae8314233a25
Bernhard Fürst <bernhard.fuerst@fuerstnet.de>
2016-05-26 18:51:40 +0200

    First working version using the VAT table entries to sum up values by UStVA Kennziffer which is expected in column Gr2.

--- UStVA/ch.banana.apps.ustvagermany.js
+++ UStVA/ch.banana.apps.ustvagermany.js
@@ -28,6 +28,14 @@
 // @includejs = xmlwriter.js
 // @includejs = geierlein.js
 
+/*
+  Show messages for debugging:
+  - Show message in message pane only, not as alert:
+    Banana.application.showMessages(false);
+  - Show objects and arrays:
+    Banana.document.addMessage(JSON.stringify(variable, null, 4));
+ */
+
 function exec() {
 
   // Show debug output in the Message pane of the current window only.
@@ -38,32 +46,12 @@ function exec() {
 
   // Cancel if user did not enter a Periode.
   if (!period) {
-    Banana.document.addMessage(JSON.stringify('Bitte wählen Sie einen Zeitbereich.', null, 4));
+    Banana.document.addMessage('Bitte wählen Sie einen Zeitbereich.');
     return;
   }
 
-
-
   // TODO: Make sure we get a known State from the AccountingDataBase.
   var land = getLandIdByName(Banana.document.info('AccountingDataBase','State'));
-
-  // Get netto Umsatz values for 19% and 7%.
-  // Must be positive and Integers.
-  var vatU19 = Banana.document.vatCurrentBalance('U19', period.start, period.end);
-  var vatTaxableU19 = Math.abs(vatU19.vatTaxable);
-  vatTaxableU19 = parseInt(vatTaxableU19, 10);
-
-  var vatU7 = Banana.document.vatCurrentBalance('U7', period.start, period.end);
-  var vatTaxableU7 = Math.abs(vatU7.vatTaxable);
-  vatTaxableU7 = parseInt(vatTaxableU7, 10);
-
-  var vatUE19 = Banana.document.vatCurrentBalance('UE19', period.start, period.end);
-  var vatTaxableUE19 = Math.abs(vatUE19.vatTaxable);
-  vatTaxableUE19 = parseInt(vatTaxableUE19, 10);
-
-  // Get added Vorsteuer.
-  var vatV = Banana.document.vatCurrentBalance('V*', period.start, period.end);
-
 
   // Get data from AccountingDataBase.
   // TODO: User friendly error handling for missing or wrong values.
@@ -77,51 +65,65 @@ function exec() {
     land: '13',
     steuernummer: Banana.document.info('AccountingDataBase','FiscalNumber'),
     jahr: '2016',
-    zeitraum: '4',
-    kz81: vatTaxableU19,
-    kz86: vatTaxableU7,
-    kz89: vatTaxableUE19,
-    kz93: '',
-    kz66: vatV.vatAmount,
-    kz61: '',
-    kz62: '',
-    kz39: '',
-    kz83: ''
+    zeitraum: '4'
   };
+
+  var kennziffern = getGermanKennziffernFromVatTable();
+  for (var i in kennziffern) {
+    var kz = kennziffern[i];
+    var vatCodes = getVatCodesByGr2(kz);
+    var vat = Banana.document.vatCurrentBalance(vatCodes, period.start, period.end);
+    if (vat.rowCount) {
+      var taxable = vat.vatTaxable * -1;
+      data['kz'+kz] = parseInt(taxable, 10);
+
+      var pair = getPairwiseKennziffer(kz);
+      if (pair) {
+        var vatAmount = vat.vatAmount * -1;
+        data['kz'+pair] = parseInt(vatAmount, 10);
+      }
+    }
+  }
+
+  // Get added Vorsteuer.
+  var vatV = Banana.document.vatCurrentBalance('V*', period.start, period.end);
+
+  Banana.application.showMessages(false);
+  Banana.document.addMessage(JSON.stringify(data, null, 4));
 
   var ustva = new geierlein.UStVA();
 
   // Format data.
   for(var key in data) {
-      if(data[key] === '') {
-          /* Skip empty fields, especially the GUI version simply adds them
-             all to explicitly state that the field was left empty. */
-          continue;
-      }
-      if(key.substr(0, 2) === 'kz'
-         || key === 'land') {
-          /* Information belongs to the UStVA document and is numeric. */
-          ustva[key] = +data[key] // .replace(',', '.');
-      } else if(key === 'steuernummer'
-         || key === 'jahr'
-         || key === 'zeitraum') {
-          /* Information belongs to the UStVA document and is a string. */
-          ustva[key] = data[key];
-      } else {
-          ustva.datenlieferant[key] = data[key];
-      }
+    if(data[key] === '') {
+      /* Skip empty fields, especially the GUI version simply adds them
+         all to explicitly state that the field was left empty. */
+      continue;
+    }
+    if(key.substr(0, 2) === 'kz'
+       || key === 'land') {
+        /* Information belongs to the UStVA document and is numeric. */
+        ustva[key] = +data[key] // .replace(',', '.');
+    } else if(key === 'steuernummer'
+       || key === 'jahr'
+       || key === 'zeitraum') {
+        /* Information belongs to the UStVA document and is a string. */
+        ustva[key] = data[key];
+    } else {
+        ustva.datenlieferant[key] = data[key];
+    }
   }
 
   // Calculate resulting value for UStVA.
   if(ustva.kz83 === undefined) {
-      ustva.kz83 = +ustva.calculateKz83().toFixed(2);
+    ustva.kz83 = +ustva.calculateKz83().toFixed(2);
   }
 
   // Validate data.
   // TODO: User friendly error handling for missing Stammdaten or wrong Kennzahlen.
   var validationResult = ustva.validate();
   if(validationResult !== true) {
-    Banana.document.addMessage(JSON.stringify('Fehler bei der Validierung der Daten.', null, 4));
+    Banana.document.addMessage('Fehler bei der Validierung der Daten.');
     return;
   }
 
@@ -208,4 +210,107 @@ function getPeriodSettings() {
     return;
   }
   return scriptform;
+}
+
+/**
+ * Get VAT Codes from the VAT table by value of the Gr2 column.
+ *
+ * The Gr2 value usually is a german UStVA Kennziffer.
+ *
+ * @param  {int}    gr2 Value of Gr2 column in the VAT table.
+ * @return {string}     One VAT code or many VAT codes devided by "|" (pipe).
+ */
+function getVatCodesByGr2(gr2) {
+  var tableVatCodes = Banana.document.table("VatCodes");
+  if (!tableVatCodes) {
+    return '';
+  }
+
+  // Get rows of VAT table containing the submitted Gr2 value.
+  // Using an anonymous function as callback to allow adding the gr2 parameter
+  // to the callback function.
+  var rows = tableVatCodes.findRows(function(row, index, table) {return filterVatTableRowsByGr2(row, index, table, gr2)} );
+  if (!rows) {
+    return '';
+  }
+
+  // Loop through results and get the VatCode value.
+  var vatCodes = [];
+  for (var i in rows) {
+    vatCodes.push(rows[i].value("VatCode"));
+  }
+
+  // Return VatCodes as string devided by "|".
+  return vatCodes.join('|');
+}
+
+/**
+ * Call back for findRows() to filter VAT table for a value in the Gr2 column.
+ *
+ * @param  {int}    gr2   Value of Gr2 column in the VAT table.
+ * @return {boolean}      True if Gr2 column of row matches, false if not.
+ */
+function filterVatTableRowsByGr2(row, index, table, gr2) {
+  var rowGr2 = row.value("Gr2");
+
+  if (gr2 == '*' && rowGr2.length > 0) {
+    return true;
+  }
+
+  if (gr2 == rowGr2) {
+    return true;
+  }
+
+  return false;
+}
+
+function getGermanKennziffernFromVatTable() {
+  var tableVatCodes = Banana.document.table("VatCodes");
+  if (!tableVatCodes) {
+    return '';
+  }
+
+  // Get rows of VAT table containing the submitted Gr2 value.
+  // Using an anonymous function as callback to allow adding the gr2 parameter
+  // to the callback function.
+  var rows = tableVatCodes.findRows(function(row, index, table) {return filterVatTableRowsByGr2(row, index, table, '*')} );
+  if (!rows) {
+    return '';
+  }
+
+  var kennziffern = new Array();
+  for (var i in rows) {
+    var value = rows[i].value("Gr2");
+    if (value.length > 0 && kennziffern.indexOf(value) < 0) {
+      kennziffern.push(value);
+    }
+  }
+
+  return kennziffern;
+}
+
+/**
+ * Get pair
+ *
+ * @param  {[type]} kz [description]
+ * @return {[type]}    [description]
+ */
+function getPairwiseKennziffer(kz) {
+  var pairs = {
+    35: 36,
+    76: 80,
+    95: 98,
+    94: 96,
+    46: 47,
+    52: 53,
+    73: 74,
+    78: 79,
+    84: 85
+  }
+
+  if (pairs[kz]) {
+    return pairs[kz];
+  }
+
+  return false;
 }
